7aea556ccf08ccc6f5d2aa57ff3b78479cfbf9fa
tburgin <tom.burgin@mac.com>
2016-03-16 12:16:20 -0400

    Add KeychainMinderAgent. Add Pods.

---
+++ KeychainMinderAgent/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KeychainMinderAgent
+//
+//  Created by bur on 3/16/16.
+//  Copyright © 2016 Google Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
